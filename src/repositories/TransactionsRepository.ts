b399819ff68ad901e180061484a8ca94295aef5e
Lucas Camargo <lucasn.camargo@gmail.com>
2020-05-28 21:38:51 -0300

    Transacoes ok

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,4 +1,4 @@
-import { EntityRepository, Repository } from 'typeorm';
+import { EntityRepository, Repository, getRepository } from 'typeorm';
 
 import Transaction from '../models/Transaction';
 
@@ -11,7 +11,21 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const transactionsRepository = getRepository(Transaction);
+    const transactions = await transactionsRepository.find();
+
+    const [income, outcome] = transactions.reduce(
+      ([totalIncome, totalOutcome], transaction) => {
+        const { type, value } = transaction;
+        return [
+          type === 'income' ? totalIncome + value : totalIncome,
+          type === 'outcome' ? totalOutcome + value : totalOutcome,
+        ];
+      },
+      [0, 0],
+    );
+    const total = income - outcome;
+    return { income, outcome, total };
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,60 @@
-// import AppError from '../errors/AppError';
+import { getRepository, getCustomRepository } from 'typeorm';
+import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+    const categoryRepository = getRepository(Category);
+
+    if (type === 'outcome') {
+      const { total } = await transactionsRepository.getBalance();
+      if (value > total) {
+        throw new AppError(
+          'Your transaction amount exceeds the available balance.',
+        );
+      }
+    }
+
+    const categoryExists = await categoryRepository.findOne({
+      where: { title: category },
+    });
+
+    let category_id = categoryExists?.id;
+
+    if (!category_id) {
+      const newCategory = categoryRepository.create({
+        title: category,
+      });
+      await categoryRepository.save(newCategory);
+      category_id = newCategory.id;
+    }
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+      category_id,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
